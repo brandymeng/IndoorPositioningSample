729626e99af8587c2f65d496dc6343c2fa9763f5
Ruben Xifré <rxifre@identitat.com>
2015-04-15 12:54:50 +0200

    Initial Commit

---
+++ IndoorPositioningSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IndoorPositioningSample
+//
+//  Created by Ruben Xifré on 15/4/15.
+//  Copyright (c) 2015 Ruben Xifré. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
